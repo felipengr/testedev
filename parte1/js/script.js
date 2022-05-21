a522cb69ae09700fd3453628674afa894c64137b
Felipe Nogueira <felipenogueira.94@gmail.com>
2022-05-21 11:17:11 -0300

    layout desktop

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,14 @@
-/* Coloque o JS neste arquivo */
+$(document).ready(function(){
+    bannerHome();
+});
+
+function bannerHome(){
+    $('.banner-box').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1500,
+        dots: false,
+        adaptiveHeight: true
+    });
+};
