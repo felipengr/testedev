9497db9f1c62f6b1381e9d9fa4c43839479b84f7
Felipe Nogueira <felipenogueira.94@gmail.com>
2022-05-25 19:25:32 -0300

    layout mobile

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
     bannerHome();
+    slickNavMobile();
+    slickShelf();
 });
 
 function bannerHome(){
     $('.banner-box').slick({
-        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1500,
+        dots: false,
+        adaptiveHeight: true
+    });
+
+    $('.banner-box-mobile').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 1500,
@@ -12,3 +21,38 @@ function bannerHome(){
         adaptiveHeight: true
     });
 };
+
+function slickNavMobile() {
+    if (window.innerWidth < 1024) {
+        $('.pics').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 500,
+            dots: false,
+            arrows: false,
+            adaptiveHeight: true
+        })
+    }
+};
+
+function slickShelf() {
+    if (window.innerWidth < 1024) {
+        $('.shelf .products').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 1500,
+            dots: false,
+            arrows: true,
+            adaptiveHeight: true
+        });
+
+        $('.bar-benefits').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 500,
+            dots: false,
+            arrows: false,
+            adaptiveHeight: true
+        })
+    }
+}
